8fad540f06f8af6c32b279d65ea004fcc8292af7
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2024-03-16 23:19:25 +0100

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    // enable hydration mismatch details in production build
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true,
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
